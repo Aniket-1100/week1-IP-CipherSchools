391669a9f7d0793fa9636a270f386de13b0b07b7
Aniket-1100 <114793936+Aniket-1100@users.noreply.github.com>
2022-11-28 13:32:13 +0530

    Add files via upload

---
+++ class 22-Int() function in python-Cipher School.py.py
@@ -0,0 +1,17 @@
+number_one = input("enter first number ")
+number_two = input("enter second number ")
+total = number_one + number_two
+print("total is " + total)
+
+
+number_three = int(input("enter third number "))
+number_four = int(input("enter fourth nuber "))
+total = number_three + number_four
+print("total is " + str(total))
+print("total is " + total)
+
+
+number1 = str(4)
+number2 = float("44")
+number3 = int("33")
+print(number2 + number3)
